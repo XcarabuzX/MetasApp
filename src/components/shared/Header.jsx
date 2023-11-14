51b6d9d15a1e44c9704bd02ebc8fbbb6ccbc9258
XcarabuzX <xcarabuzx@gmail.com>
2023-11-14 15:30:28 -0300

    Aplicando estilos al header, solucion de importacion svg

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,12 +1,17 @@
+import './Header.css';
+import Logo  from '../../assets/img/logo.svg?react';
+import Perfil from '../../assets/img/perfil.svg?react';
 function Header (){
     return(
-        <div>
-            <div>
-                <p>logo</p>
-                <a href="/">Metas App</a>
+        <div className="header">
+            <div className="contenedor">
+                <Logo className="logo"/>
+                <a href="/" className="titulo">Metas App</a>
             </div>
             <nav>
-                <a href="/perfil">Perfil</a>
+                <a href="/perfil" className="vinculo">
+                    <Perfil className="icono"/>
+                </a>
             </nav>
         </div>
     );
