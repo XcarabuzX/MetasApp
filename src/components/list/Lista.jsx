30d3c56d5468e67cf1851daebf4541384cbb0a97
XcarabuzX <xcarabuzx@gmail.com>
2023-11-23 12:04:19 -0300

    Agregando condicoin cuando no hay metas

--- src/components/list/Lista.jsx
+++ src/components/list/Lista.jsx
@@ -5,9 +5,16 @@ import { Outlet } from "react-router-dom";
 
 function Lista() {
     const [estado] = useContext(Contexto);
-
-
-
+    if(estado.orden.length === 0){
+        return(
+            <div className="flex flex-col items-center justify-center h-full">
+                <div className="mx-auto text-center text-gray-500">
+                    <h3>Esto esta un poco vacio</h3>
+                    <p>¡Agrega una meta!</p>
+                </div>
+            </div>
+        );
+    }
     return (
         <>
             {estado.orden.map(id => <Meta key={id} {...estado.objetos[id]}/>)}
